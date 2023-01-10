e322a2d1b3f1ad2c6ef460f41948d0df7290b4fc
Albert Woloszyn <awoloszyn@gmail.com>
2023-01-10 21:29:46 +0000

    Added .env to the project

--- e2e/google test.spec.js
+++ e2e/google test.spec.js
@@ -1,4 +1,6 @@
 import { test, expect } from '@playwright/test';
+require('dotenv').config();
+
 
 test.beforeEach(async ({ page }, testInfo) => {
   console.log(`Running ${testInfo.title}`);
@@ -13,8 +15,9 @@ test('Google Page | Tools element present and spelled correctly', async ({ page 
 
   await page.getByRole('button', { name: 'Accept all' }).click();
   await page.getByRole('combobox', { name: 'Search' }).click();
-  await page.getByRole('combobox', { name: 'Search' }).fill('test');
-  await page.getByRole('listitem').filter({ hasText: 'testDelete' }).getByRole('option', { name: 'test' }).click();
+  await page.getByRole('combobox', { name: 'Search' }).fill(process.env.EMAIL);
+  //await page.getByRole('listitem').filter({ hasText: 'testDelete' }).getByRole('option', { name: process.env.EMAIL }).click();
+  await page.keyboard.press('Enter');
   //verify that the searchbar is visible
   await (tools).waitFor();
   await (searchBox).waitFor();
@@ -25,7 +28,7 @@ test('Google Page | Tools element present and spelled correctly', async ({ page 
   //verify thet the search box is visible
     expect(searchBox).toBeVisible();
   //verify that the toolbox element is visible
-    expect(toolsx).toBeVisible();
+    expect(tools).toBeVisible();
   //verify that the toolbox element contains name of 'Tools'
     expect(tools).toContainText('Tools');
   
